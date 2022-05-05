de64931410c0e7e9f9845fc95c221ceaef8a0cd7
Carlos Ortega <carlos.ortega.ast0318@gmail.com>
2022-05-04 20:01:36 -0500

    nuevo modelo mission

--- prisma/seed.js
+++ prisma/seed.js
@@ -51,3 +51,44 @@ const prisma = new PrismaClient();
     await prisma.$disconnect();
   }
 })(); 
+
+(async function main() {
+  try {
+    const mission1 = await prisma.mission.upsert({
+      where: { name: 'Mission 1' },
+      update: {},
+      create: {
+        name: 'Mission 1',
+        lang: 'Node',
+				missionCommander: 'missionCommander1',
+      },
+    });
+
+    const mission2 = await prisma.mission.upsert({
+      where: { name: 'Mission 2' },
+      update: {},
+      create: {
+        name: 'Mission 2',
+        lang: 'Java',
+				missionCommander: 'missionCommander1',
+      },
+    });
+
+    const mission3 = await prisma.mission.upsert({
+      where: { name: 'Mission 3' },
+      update: {},
+      create: {
+        name: 'Mission 3',
+        lang: 'Node',
+				missionCommander: 'missionCommander1',
+      },
+    });
+
+    console.log('Create 3 explorers');
+  } catch(e) {
+    console.error(e);
+    process.exit(1);
+  } finally {
+    await prisma.$disconnect();
+  }
+})(); 
